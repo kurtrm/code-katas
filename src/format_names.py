deb5777239e50777035ee23beb8af228ef69b432
Kurt <kurtrm@gmail.com>
2017-05-19 16:58:35 -0700

    Finished format_names kata.

---
+++ src/format_names.py
@@ -0,0 +1,25 @@
+"""Kata: Your order, please - Return ordered string based on number in word
+
+#1 Best Practices Solution marceltschoppch & Others
+
+def namelist(names):
+    if len(names) > 1:
+        return '{} & {}'.format(', '.join(name['name'] for name in names[:-1]), 
+                                names[-1]['name'])
+    elif names:
+        return names[0]['name']
+    else:
+        return ''
+"""
+
+
+def namelist(names):
+    if len(names) == 0:
+        return ''
+    elif len(names) == 1:
+        return names[0]['name']
+    elif len(names) == 2:
+        return ' & '.join([dicty['name'] for dicty in names])
+    first_few = ', '.join([dicty['name'] for dicty in names[:-2]])
+    last_two = ' & '.join([dicty['name'] for dicty in names[-2:]])
+    return ', '.join([first_few, last_two])
