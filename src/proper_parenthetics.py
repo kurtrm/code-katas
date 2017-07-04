1d13fda0095d4cc10b877460c964aefc34152244
Kurt <kurtrm@gmail.com>
2017-07-03 19:54:46 -0700

    Tests passing, refactored a bit.

--- src/proper_parenthetics.py
+++ src/proper_parenthetics.py
@@ -5,21 +5,24 @@ broken: -1 ')'
 open: 1 '('
 
 """
-import re
 
 
 def parenthetics(parentheses):
     """Process the set of parentheses."""
-    if isinstance(parentheses, str):
+    if not isinstance(parentheses, str):
         raise TypeError('Please provide a string.')
-    if re.search(r'[^\(\)]', parentheses):
-        raise ValueError('Please provide either ( or ) only.')
     count = 0
+    contains_paren = False
     for paren in parentheses:
-        if paren == '(':
-            count += 1
-        elif paren == ')':
-            count -= 1
-        if count < 0:
-            return -1
-    return 0 if count == 0 else 1
+        if paren in '()':
+            contains_paren = True
+            if paren == '(':
+                count += 1
+            else:
+                count -= 1
+            if count < 0:
+                return -1
+    if contains_paren:
+        return 0 if count == 0 else 1
+    else:
+        return 'No parentheses in this string.'
--- src/test_parenthetics.py
+++ src/test_parenthetics.py
@@ -12,7 +12,14 @@ PARAMS_TABLE = [
     ('((()()(()((())))(', 1),
     ('(()))(()()()(()(())(()))', -1),
     ('()()()()((((())))))(((()(()()', -1),
-    ('((()))()()((())())()())()', 0)
+    ('((()))()()((())())()())()', -1),
+    ('((()))()()((())())()()((()))', 0),
+    ('(((a))()((()', 1),
+    ('(((a))()(fjdksla;dkjfh(()', 1),
+    ('asdfjdksla;woei234387371', 'No parentheses in this string.'),
+    ('trheadkfej)', -1),
+    ('(as;dlkajfa;slk', 1),
+
 ]
 
 
@@ -21,3 +28,10 @@ def test_against_params(parentheses, result):
     """Test against the generated tests."""
     from proper_parenthetics import parenthetics
     assert parenthetics(parentheses) == result
+
+
+def test_type_error():
+    """Test that we get the type error if anything but a string is supplied."""
+    from proper_parenthetics import parenthetics
+    with pytest.raises(TypeError):
+        parenthetics(2000)
