fb13f3d93a1fb1adfb1426fcaf537ed73567a30e
Kurt <kurtrm@gmail.com>
2017-07-03 19:25:52 -0700

    Finished the function.

--- src/proper_parenthetics.py
+++ src/proper_parenthetics.py
@@ -1,6 +1,9 @@
 """Function to process a set of parentheses and return if
 
-it's balanced, broken, or open.
+balanced: 0 '()'
+broken: -1 ')'
+open: 1 '('
+
 """
 import re
 
@@ -9,5 +12,14 @@ def parenthetics(parentheses):
     """Process the set of parentheses."""
     if isinstance(parentheses, str):
         raise TypeError('Please provide a string.')
-    if re.search(r'[^\()]', parentheses):
+    if re.search(r'[^\(\)]', parentheses):
         raise ValueError('Please provide either ( or ) only.')
+    count = 0
+    for paren in parentheses:
+        if paren == '(':
+            count += 1
+        elif paren == ')':
+            count -= 1
+        if count < 0:
+            return -1
+    return 0 if count == 0 else 1
