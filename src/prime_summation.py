c4432749b74571a0a85f551dd62ad2d4f1c7fde2
Kurt Maurer <kurtrm@gmail.com>
2018-08-31 14:17:45 -0700

    need to improve summationfoprimes.

---
+++ src/prime_summation.py
@@ -0,0 +1,26 @@
+"""
+Summation of primes kata.
+"""
+import sys
+
+
+def O_n_squared_summationOfPrimes(primes):
+    """
+    return the sum of primes of a number.
+    """
+    prime_sum = 0
+    for number in range(2, primes + 1):
+        mod_zero = False
+        for digit in range(2, number + 1):
+            if digit == number and mod_zero is False:
+                prime_sum += number
+            elif number % digit == 0:
+                mod_zero = True
+
+    return prime_sum
+
+
+if __name__ == '__main__':
+    x = int(sys.argv[1])
+    phrase = f'Summation of primes of {x} is {O_n_squared_summationOfPrimes(x)}'
+    print(phrase)
