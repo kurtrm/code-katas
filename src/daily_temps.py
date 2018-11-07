815aa51bdbe19b3ec4e7d2a799880fc601c89995
Kurt Maurer <kurtrm@gmail.com>
2018-11-07 14:31:32 -0800

    Solved suing two stacks, but exceeding time limits.

--- src/daily_temps.py
+++ src/daily_temps.py
@@ -16,7 +16,38 @@ def slow_daily_temps(T):
     return days
 
 
-def fast_daily_temps(T):
+def stack_daily_temps(T):
     """
+    uses two stacks to solve the problem,
+    still having complexity issues
     """
-    
+    days = []
+    stack = []
+    reversed_T = T[::-1]
+    ref_day = reversed_T.pop()
+    next_day = reversed_T.pop()
+    num_days = 0
+    while len(days) < len(T):
+        try:
+            num_days += 1
+            if next_day > ref_day:
+                days.append(num_days)
+                reversed_T.append(next_day)
+                while stack:
+                    reversed_T.append(stack.pop())
+                ref_day = reversed_T.pop()
+                next_day = reversed_T.pop()
+                num_days = 0
+            else:
+                stack.append(next_day)
+                next_day = reversed_T.pop()
+        except IndexError:
+            days.append(0)
+            num_days = 0
+            reversed_T.append(next_day)
+            while stack:
+                reversed_T.append(stack.pop())
+            if reversed_T:
+                ref_day = reversed_T.pop()
+                next_day = reversed_T.pop()
+    return days
